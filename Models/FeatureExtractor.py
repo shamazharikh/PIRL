21d0b5407df1d16b9ef0cc7b3712f73bed3f6bec
Mazhar WPC <mazharshaikh94@gmail.com>
2020-01-03 09:36:46 +0900

    Added Intermediate Feature Extractor

---
+++ Models/FeatureExtractor.py
@@ -0,0 +1,13 @@
+class IntermediateLayerGetter(object):
+    """
+    Saves intermediate layers in a dictionary
+    """
+    def __init__(self, net, return_layers=[]):
+        self.activations = {}
+        for name, module in net.named_modules():
+            if name in return_layers:
+                module.register_forward_hook(self.get_hook(name))
+    def get_hook(self, name):
+        def hook(module, input, output):
+            self.activations[name] = output
+        return hook
