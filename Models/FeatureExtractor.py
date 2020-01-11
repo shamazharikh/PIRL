eddd1bad83098827a6f513b4343394c03d38f0b9
Mazhar <mazharshaikh94@gmail.com>
2020-01-11 19:42:19 +0900

    ILG as normal object

--- Models/FeatureExtractor.py
+++ Models/FeatureExtractor.py
@@ -1,10 +1,10 @@
 import torch.nn as nn
-class IntermediateLayerGetter(nn.Module):
+class IntermediateLayerGetter(object):
     """
     Saves intermediate layers in a dictionary
     """
     def __init__(self, net, return_layers=[]):
-        super(IntermediateLayerGetter, self).__init__()
+        # super(IntermediateLayerGetter, self).__init__()
         self.activations = {}
         self.output_sizes = {}
         for name, module in net.named_modules():
